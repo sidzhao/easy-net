07894f5980ee1dba8d7bd4fa9b3a70f6bcff2466
Sid Zhao <sid.zhao@webezi.com.au>
2021-02-23 17:55:06 +0800

    coding

--- test/EasyNet.Tests/EasyNetLifetimeTest.cs
+++ test/EasyNet.Tests/EasyNetLifetimeTest.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using EasyNet.Extensions.DependencyInjection;
 using EasyNet.Uow;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
