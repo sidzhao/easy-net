be84eb613eeb19e89e51f79ff578702152552fc2
sidzhao <sidzhao@outlook.com>
2021-03-15 17:38:05 +0800

    coding

---
+++ test/EasyNet.MySql.Tests/ServiceCollectionExtensionsTest.cs
@@ -0,0 +1,30 @@
+﻿using EasyNet.CommonTests;
+using EasyNet.Data;
+using EasyNet.Extensions.DependencyInjection;
+using EasyNet.MySql.Data;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace EasyNet.MySql.Tests
+{
+    public class ServiceCollectionExtensionsTest : DependencyInjectionTest
+    {
+        [Fact]
+        public void TestAddServices()
+        {
+            // Arrange
+            var services = new ServiceCollection();
+            services.AddSingleton(CommonTest.GetHostingEnvironment());
+
+            // Act
+            services
+                .AddEasyNet(x =>
+                {
+                    x.UseMySql("ConnectionString");
+                });
+
+            // Assert
+            AssertSpecifiedServiceTypeAndImplementationType<IDbConnectorCreator, MySqlConnectorCreator>(services, ServiceLifetime.Singleton);
+        }
+    }
+}
